7932677506e48e2a6cc1e8be50d48582a3bac9ed
Марков Алексей <aleks-mark-dzr@yandex.ru>
2023-07-06 16:13:41 +0300

    correcting code

--- 14_Flow/app/src/main/java/com/example/a14_flow/main.kt
+++ 14_Flow/app/src/main/java/com/example/a14_flow/main.kt
@@ -1,32 +1,48 @@
 package com.example.a14_flow
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.toList
 import kotlinx.coroutines.flow.*
 import kotlin.random.Random
 
 fun main() {
     runBlocking {
         launch {
+            LotoCardGenerator.usedNumber.collect {
+                print("*$it ")
+//                delay(500)
+            }
+        }
+        launch {
             LotoCardGenerator.lotoNumber.collect {
                 print("$it ")
+//                delay(500)
             }
         }
     }
 }
 
 
+@OptIn(FlowPreview::class)
 object LotoCardGenerator {
     private val scope = CoroutineScope(Job() + Dispatchers.Default)
     private val _lotoNumber = MutableSharedFlow<Int>()
-//    private val _usedNumber = MutableSharedFlow<Int>()
+    private val _usedNumber = MutableSharedFlow<Int>()
     val lotoNumber = _lotoNumber.asSharedFlow()
-//    val usedNumber = _usedNumber.asSharedFlow()
+    val usedNumber = _usedNumber.asSharedFlow()
 
     init {
         scope.launch {
             repeat(15) {
-                _lotoNumber.emit(Random.nextInt(1, 90))
-                delay(200)
+                val randomNumber = Random.nextInt(1, 90)
+                _lotoNumber.emit(randomNumber)
+                _usedNumber.emit(randomNumber)
+
+//                if (!(randomNumber in _usedNumber.produceIn(scope).toList())) {
+//                    _lotoNumber.emit(randomNumber)
+//                    _usedNumber.asSharedFlow()
+//                }
+                delay(100)
             }
         }
     }
