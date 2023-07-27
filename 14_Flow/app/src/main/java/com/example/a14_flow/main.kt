5d03519fc295e2e4a41a1cf9de3d039755341e77
Марков Алексей <aleks-mark-dzr@yandex.ru>
2023-07-27 16:39:42 +0300

    correct 14_Flow_GameLoto

--- 14_Flow/app/src/main/java/com/example/a14_flow/main.kt
+++ 14_Flow/app/src/main/java/com/example/a14_flow/main.kt
@@ -1,49 +1,73 @@
 package com.example.a14_flow
 
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.toList
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.random.Random
 
-fun main() {
-    runBlocking {
-        launch {
-            LotoCardGenerator.usedNumber.collect {
-                print("*$it ")
-//                delay(500)
+data class Player(val name: String, val card: List<List<Int>>)
+
+class LotoGame(private val players: List<Player>) {
+
+    private val numbersFlow = MutableSharedFlow<Int>()
+
+    init {
+        // Генерируем случайные числа и отправляем их в Flow
+        generateRandomNumbers()
+    }
+
+    private fun generateRandomNumbers() = runBlocking {
+        coroutineScope {
+            launch {
+                repeat(90) {
+                    // Генерируем случайное число от 1 до 90
+                    val randomNumber = Random.nextInt(1, 91)
+                    numbersFlow.emit(randomNumber)
+                    delay(1000) // Задержка в 1 секунду между числами
+                }
             }
         }
-        launch {
-            LotoCardGenerator.lotoNumber.collect {
-                print("$it ")
-//                delay(500)
+    }
+
+    fun startGame() = runBlocking {
+        coroutineScope {
+            players.forEach { player ->
+                launch {
+                    // Следим за числами из Flow и проверяем на выигрыш
+                    numbersFlow.collect { number ->
+                        if (player.card.flatten().contains(number)) {
+                            println("${player.name} выиграл(а) число $number в ${System.currentTimeMillis()}")
+                        }
+                    }
+                }
             }
         }
     }
 }
 
+fun main() {
+    val player1 = Player("Игрок 1", generateRandomCard())
+    val player2 = Player("Игрок 2", generateRandomCard())
 
-@OptIn(FlowPreview::class)
-object LotoCardGenerator {
-    private val scope = CoroutineScope(Job() + Dispatchers.Default)
-    private val _lotoNumber = MutableSharedFlow<Int>()
-    private val _usedNumber = MutableSharedFlow<Int>()
-    val lotoNumber = _lotoNumber.asSharedFlow()
-    val usedNumber = _usedNumber.asSharedFlow()
+    val game = LotoGame(listOf(player1, player2))
+    game.startGame()
+}
 
-    init {
-        scope.launch {
-            repeat(15) {
-                val randomNumber = Random.nextInt(1, 90)
-                _lotoNumber.emit(randomNumber)
-                _usedNumber.emit(randomNumber)
-
-//                if (!(randomNumber in _usedNumber.produceIn(scope).toList())) {
-//                    _lotoNumber.emit(randomNumber)
-//                    _usedNumber.asSharedFlow()
-//                }
-                delay(100)
-            }
-        }
+fun generateRandomCard(): List<List<Int>> {
+    val card = mutableListOf<List<Int>>()
+    val numbers = mutableListOf<Int>()
+    for (i in 1..90) {
+        numbers.add(i)
     }
+    numbers.shuffle()
+
+    repeat(3) {
+        val row = numbers.subList(it * 9, (it + 1) * 9).sorted().take(5)
+        card.add(row)
+    }
+
+    return card
 }
